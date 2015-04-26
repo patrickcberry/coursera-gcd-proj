8f31495198c8077b3964d1330806b17ecfbc473e
Patrick Berry <patrickcberry@gmail.com>
2015-04-26 19:48:36 +1000

    Final commit prior to submission

--- run_analysis.R
+++ run_analysis.R
@@ -17,12 +17,11 @@ y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 x_train <- read.table("./UCI HAR Dataset/train/x_train.txt")
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
 
-# Set the feature names
+# Set the feature names in the raw data files (required for development only)
 names(x_test) <- features$V2
 names(x_train) <- features$V2
 
 # Determine the features required - mean and std. Subset based on required features
-
 required_features <- features[c(grep( "-mean", features$V2 ),grep( "-std", features$V2 )),]
 req_x_test <- x_test[,required_features$V1]
 names(req_x_test) <- required_features$V2
@@ -30,11 +29,11 @@ names(req_x_test) <- required_features$V2
 req_x_train <- x_train[,required_features$V1]
 names(req_x_train) <- required_features$V2
 
+# Set up merging files
 merge_test <- req_x_test
 merge_train <- req_x_train
 
 # Merge activites
-
 merge_test$Activity_Index <- merge( activity_labels, y_test, by = "V1" )$V1
 #merge_test$Activity_Label <- merge( activity_labels, y_test, by = "V1" )$V2
 
@@ -42,12 +41,10 @@ merge_train$Activity_Index <- merge( activity_labels, y_train, by = "V1" )$V1
 #merge_train$Activity_Label <- merge( activity_labels, y_train, by = "V1" )$V2
 
 # Merge Subject
-
 merge_test$Subject_ID <- subject_test$V1
 merge_train$Subject_ID <- subject_train$V1
 
 # Merge test and train
-
 merge_all <- rbind( merge_test, merge_train )
 
 # Part 5. 
